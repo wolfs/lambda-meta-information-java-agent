f30660e1d2a82dc0e93938551dce7e97fc26c8b8
Stefan Wolf <wolf@gradle.com>
2018-08-23 19:00:34 +0200

    Fix project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "ambda-meta-information-java-agent"
+rootProject.name = "lambda-meta-information-java-agent"
 include("agent", "instrumented-code")
 
 enableFeaturePreview("IMPROVED_POM_SUPPORT")
