5afe928973403ea3a253e1eddf6852086ef26216
Stefan Wolf <wolf@gradle.com>
2018-08-23 18:06:14 +0200

    Change project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "lambda-instrumentation-agent"
+rootProject.name = "ambda-meta-information-java-agent"
 include("agent", "instrumented-code")
 
 enableFeaturePreview("IMPROVED_POM_SUPPORT")
